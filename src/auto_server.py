d95fef3118327f3d2a376a79ba6d3ed0d74bf9e8
kazuki.masumura <kazuki.masumura@adminnoMacBook-puro-8.local>
2018-08-31 12:59:00 +0900

    add auto

--- src/auto_server.py
+++ src/auto_server.py
@@ -1,10 +1,11 @@
 import subprocess
+import os
 import time
 
 
 print("start")
-cmd = "cd -"
-subprocess.call(cmd.split())
+os.chdir("../")
+os.chdir("../")
 cmd = "rcssserver"
 subprocess.Popen(cmd.split())
 time.sleep(10)
